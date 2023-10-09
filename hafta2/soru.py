a36d5500aa99a5c3335a3d30497e564570f4b785
Caner BALIM <cnrblm@gmail.com>
2023-10-09 10:29:44 +0300

    hafta 2

---
+++ hafta2/soru.py
@@ -0,0 +1,23 @@
+"""
+klavyeden alınan kullanıcı adı ve
+şifre bilgilerine göre adsoyad.txt içerisinde
+bulunan bilgiler dogruyla ekrana
+hoşgeldin adsoyad şekilde çıktı veren
+programı kodlayınız.
+"""
+
+kullanici_adi=input("Kullanıcı adınızı giriniz: ")
+sifre=input("Şifrenizi giriniz: ")
+
+with open("adsoyad.txt","r") as dosya:
+    veriler=dosya.readlines()
+    dosya.close()
+
+for satir in veriler:
+    alanlar=satir.split(",")
+    if alanlar[0]==kullanici_adi and alanlar[1]==sifre:
+        print("Hoşgeldin",alanlar[2])
+        break
+    
+
+print("program sonlandı")
